1125047e44426e7d269bcbfff07c367b12984582
Jul Sus <Jul Sus>
2017-05-24 05:29:27 +0300

    dcs

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -7,39 +7,85 @@
 Brahma.FSharp.Examples
 ======================
 
-Documentation
+Examples of Brahma.FSharp usage. 
+
+Brahma.FSharp
+-------------
+
+Brahma.FSharp is aimed to creation tool for heterogeneous systems (multicore CPU + many GPGPUs) programming. 
+F# quotation translator is used for integration with GPGPU, and wide range of F# primitives for parallel an 
+asynchronous programming (MailboxProcessor, async, Array.Paralle, etc) simplifies utilization of heterogeneous 
+systems.
 
 <div class="row">
   <div class="span1"></div>
   <div class="span6">
     <div class="well well-small" id="nuget">
-      The Brahma.FSharp.Examples library can be <a href="https://nuget.org/packages/Brahma.FSharp.Examples">installed from NuGet</a>:
-      <pre>PM> Install-Package Brahma.FSharp.Examples</pre>
+      The Brahma.FSharp library can be <a href="https://nuget.org/packages/Brahma.FSharp">installed from NuGet</a>:
+      <pre>PM> Install-Package Brahma.FSharp</pre>
     </div>
   </div>
   <div class="span1"></div>
 </div>
 
+Features of Brahma.FSharp:
+
+ * We are aimed to translate native F# code to OpenCL with minimization of different wrappers and custom types.
+ * We use OpenCL for communication with GPU. So, you can work not only with NVIDIA hardware but with any device, 
+which support OpenCL (e.g. with AMD devices).
+ * We support tuples and structures.
+ * We can use strongly typed kernels from OpenCL code in F#.
+
 Example
 -------
 
-This example demonstrates using a function defined in this sample library.
+This example demonstrates using a function defined in this library.
+Following function computes the result of the addition of matrix 'mat2' to the matrix 'mat1'
 
 *)
-#r "Brahma.FSharp.Examples.dll"
-open Brahma.FSharp.Examples
+let Addition (mat1: array<float>) (mat2: array<float>) rows columns =
 
-printfn "hello = %i" <| Library.hello 0
+    let localWorkSize0 = 2
+    let localWorkSize1 = 2
+    let platformName = "NVIDIA*"
+    let deviceType = DeviceType.Default        
+    
+    let provider =
+        try  ComputeProvider.Create(platformName, deviceType)
+        with 
+        | ex -> failwith ex.Message
+   
+    let commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head)    
+
+    let command = 
+        <@
+            fun (rng:_2D) (a:array<_>) (b:array<_>) (c:array<_>) ->
+                let x = rng.GlobalID1
+                let y = rng.GlobalID0                
+                c.[y * columns + x] <- a.[y * columns + x] + b.[y * columns + x] 
+        @>
+
+    let a = mat1
+    let b = mat2
+    let res = Array.zeroCreate(rows * columns)
+
+    let kernel, kernelPrepare, kernelRun = provider.Compile command
+    let d =(new _2D(rows, columns, localWorkSize0, localWorkSize1))
+    kernelPrepare d a b res
+
+    let _ = commandQueue.Add(kernelRun())
+    let _ = commandQueue.Add(res.ToHost provider).Finish()
+    commandQueue.Dispose()
+    provider.Dispose()
+    provider.CloseAllBuffers()
+
+    res
 
 (**
-Some more info
+See more examples [here](tutorial.html)
 
 Samples & documentation
 -----------------------
-
-The library comes with comprehensible documentation. 
-It can include tutorials automatically generated from `*.fsx` files in [the content folder][content]. 
-The API reference is automatically generated from Markdown comments in the library implementation.
 
  * [Tutorial](tutorial.html) contains a further explanation of this sample library.
 
@@ -59,9 +105,9 @@ The library is available under Public Domain license, which allows modification 
 redistribution for both commercial and non-commercial purposes. For more information see the 
 [License file][license] in the GitHub repository. 
 
-  [content]: https://github.com/fsprojects/Brahma.FSharp.Examples/tree/master/docs/content
-  [gh]: https://github.com/fsprojects/Brahma.FSharp.Examples
-  [issues]: https://github.com/fsprojects/Brahma.FSharp.Examples/issues
-  [readme]: https://github.com/fsprojects/Brahma.FSharp.Examples/blob/master/README.md
-  [license]: https://github.com/fsprojects/Brahma.FSharp.Examples/blob/master/LICENSE.txt
+  [content]: https://github.com/YaccConstructor/Brahma.FSharp.Examples/tree/master/docs/content
+  [gh]: https://github.com/YaccConstructor/Brahma.FSharp.Examples
+  [issues]: https://github.com/YaccConstructor/Brahma.FSharp.Examples/issues
+  [readme]: https://github.com/YaccConstructor/Brahma.FSharp.Examples/blob/master/README.md
+  [license]: https://github.com/YaccConstructor/Brahma.FSharp.Examples/blob/master/LICENSE.txt
 *)
--- docs/content/tutorial.fsx
+++ docs/content/tutorial.fsx
@@ -4,16 +4,263 @@
 #I "../../bin"
 
 (**
-Introducing your project
+Usage examples
 ========================
 
-Say more
 
+
+
+ArrayExamples
+-------------
+
+ * Following function generates an array filled with Fibonacci numbers
 *)
-#r "Brahma.FSharp.Examples.dll"
-open Brahma.FSharp.Examples
 
-Library.hello 0
+let Fib length =
+    let localWorkSize = 20
+    let platformName = "NVIDIA*"
+    let deviceType = DeviceType.Default
+    
+    let provider =
+        try  ComputeProvider.Create(platformName, deviceType)
+        with 
+        | ex -> failwith ex.Message
+   
+    let commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head)    
+
+    let command = 
+        <@
+            fun (rng:_1D) (a:array<_>) ->
+                for i in 2 .. length - 1 do
+                    a.[i] <- a.[i - 1] + a.[i - 2]
+        @>
+
+    let a = Array.init (length) (fun i -> int64(1))
+
+    let kernel, kernelPrepare, kernelRun = provider.Compile command
+    let d =(new _1D(length, localWorkSize))
+    kernelPrepare d a
+
+    let _ = commandQueue.Add(kernelRun())
+    let _ = commandQueue.Add(a.ToHost provider).Finish()
+    commandQueue.Dispose()
+    provider.Dispose()
+    provider.CloseAllBuffers()
+
+    a
+(**
+ * Following function makes elements of the array arr to equal 0 in range of [firstElement, lastElement]
+*)
+
+let Nulify (arr: array<_>) length firstElement lastElement =
+    let localWorkSize = 20
+    let platformName = "NVIDIA*"
+    let deviceType = DeviceType.Default
+    
+    if firstElement >= lastElement
+    then failwith "Invalid indexes"
+
+    let provider =
+        try  ComputeProvider.Create(platformName, deviceType)
+        with 
+        | ex -> failwith ex.Message
+   
+    let commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head)    
+
+    let command = 
+        <@
+            fun (rng:_1D) (a:array<_>) ->
+                let i = rng.GlobalID0
+                if (i >= firstElement) && (i <= lastElement)
+                then a.[i] <- 0
+        @>
+
+    let kernel, kernelPrepare, kernelRun = provider.Compile command
+    let d =(new _1D(length, localWorkSize))
+    kernelPrepare d arr
+
+    let _ = commandQueue.Add(kernelRun())
+    let _ = commandQueue.Add(arr.ToHost provider).Finish()
+    commandQueue.Dispose()
+    provider.Dispose()
+    provider.CloseAllBuffers()
+
+    arr
+
+(**
+ * Following function reverts the array arr
+*)
+
+let Reflect (arr: array<_>) length =
+    let localWorkSize = 20
+    let platformName = "NVIDIA*"
+    let deviceType = DeviceType.Default
+    
+    if length <= 1 
+    then failwith "Invalid length"
+    
+    let provider =
+        try  ComputeProvider.Create(platformName, deviceType)
+        with 
+        | ex -> failwith ex.Message
+   
+    let commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head)    
+
+    let command = 
+        <@
+            fun (rng:_1D) (a:array<_>) ->
+                let i = rng.GlobalID0
+                let buff = a.[i] 
+                a.[i] <- a.[length - i]
+                a.[length - i] <- buff
+        @>
+
+    let kernel, kernelPrepare, kernelRun = provider.Compile command
+    let d =(new _1D(length, localWorkSize))
+    kernelPrepare d arr
+
+    let _ = commandQueue.Add(kernelRun())
+    let _ = commandQueue.Add(arr.ToHost provider).Finish()
+    commandQueue.Dispose()
+    provider.Dispose()
+    provider.CloseAllBuffers()
+
+    arr
+(**
+
+MatrixExamples
+--------------
+
+ * Following function computes the result of the addition of matrix 'mat2' to the matrix 'mat1'
+*)
+
+let Addition (mat1: array<float>) (mat2: array<float>) rows columns =
+
+    let localWorkSize0 = 2
+    let localWorkSize1 = 2
+    let platformName = "NVIDIA*"
+    let deviceType = DeviceType.Default        
+    
+    let provider =
+        try  ComputeProvider.Create(platformName, deviceType)
+        with 
+        | ex -> failwith ex.Message
+   
+    let commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head)    
+
+    let command = 
+        <@
+            fun (rng:_2D) (a:array<_>) (b:array<_>) (c:array<_>) ->
+                let x = rng.GlobalID1
+                let y = rng.GlobalID0                
+                c.[y * columns + x] <- a.[y * columns + x] + b.[y * columns + x] 
+        @>
+
+    let a = mat1
+    let b = mat2
+    let res = Array.zeroCreate(rows * columns)
+
+    let kernel, kernelPrepare, kernelRun = provider.Compile command
+    let d =(new _2D(rows, columns, localWorkSize0, localWorkSize1))
+    kernelPrepare d a b res
+
+    let _ = commandQueue.Add(kernelRun())
+    let _ = commandQueue.Add(res.ToHost provider).Finish()
+    commandQueue.Dispose()
+    provider.Dispose()
+    provider.CloseAllBuffers()
+
+    res
+    
+(**
+ * Following function computes the result of multiplying matrix 'mat' by constant
+*)
+let ConstantAndMatrixMultiply (mat: array<int64>) constant rows columns =
+    
+    let localWorkSize0 = 2
+    let localWorkSize1 = 2
+    let platformName = "NVIDIA*"
+    let deviceType = DeviceType.Default
+    
+    let provider =
+        try  ComputeProvider.Create(platformName, deviceType)
+        with 
+        | ex -> failwith ex.Message
+   
+    let commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head)    
+
+    let command = 
+        <@
+            fun (rng:_2D) (a:array<_>) c ->
+                let x = rng.GlobalID1
+                let y = rng.GlobalID0                
+                a.[y * columns + x] <- a.[y * columns + x] * c
+        @>
+
+    let a = mat
+    let c = constant
+
+    let kernel, kernelPrepare, kernelRun = provider.Compile command
+    let d =(new _2D(rows, columns, localWorkSize0, localWorkSize1))
+    kernelPrepare d a c
+
+    let _ = commandQueue.Add(kernelRun())
+    let _ = commandQueue.Add(a.ToHost provider).Finish()
+    commandQueue.Dispose()
+    provider.Dispose()
+    provider.CloseAllBuffers()
+
+    a
+(**
+ * Following function computes upper triangular matrix from matrix 'mat'
+*)
+
+let TriMat (mat: array<float>) rows columns =
+    
+    let localWorkSize0 = 2
+    let localWorkSize1 = 2
+    let mutable size = rows
+    let platformName = "NVIDIA*"
+    let deviceType = DeviceType.Default
+    
+    if rows <> columns
+    then failwith "Can not compute triangle matrix"
+
+    for i in 0 .. rows - 1 do
+        if mat.[i * rows + i] = 0.0
+        then failwith "Can not compute triangle matrix"
+    
+    let provider =
+        try  ComputeProvider.Create(platformName, deviceType)
+        with 
+        | ex -> failwith ex.Message
+   
+    let commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head)    
+
+    let command = 
+        <@
+            fun (rng:_2D) (a: array<_>) ->                
+                for y in 0 .. size - 2 do
+                    for x in y + 1 .. size - 1 do
+                        let k = a.[x * size + y] / a.[y * size + y]
+                        for i in 0 .. size - 1 do
+                            a.[x * size + i] <- a.[x * size + i] - a.[y * size + i] * k
+        @>
+
+    let a = mat
+
+    let kernel, kernelPrepare, kernelRun = provider.Compile command
+    let d =(new _2D(rows, columns, localWorkSize0, localWorkSize1))
+    kernelPrepare d a
+
+    let _ = commandQueue.Add(kernelRun())
+    let _ = commandQueue.Add(a.ToHost provider).Finish()
+    commandQueue.Dispose()
+    provider.Dispose()
+    provider.CloseAllBuffers()
+
+    a
+
 (**
 Some more info
 *)
